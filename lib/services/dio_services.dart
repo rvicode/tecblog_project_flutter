89684dc9ea5b52a77900dce6e00fdffffe906228
Arvin Visi <arvinvisi03@gmail.com>
2023-11-21 22:05:53 +0330

    add postMethod in dio services

--- lib/services/dio_services.dart
+++ lib/services/dio_services.dart
@@ -1,10 +1,12 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:get/get.dart';
 
 class DioServices {
+  Dio dio = Dio();
+
   Future<dynamic> getMethod(String url) async {
-    Dio dio = Dio();
     dio.options.headers['Content-Type'] = 'application/json';
     return await dio
         .get(url,
@@ -12,6 +14,20 @@ class DioServices {
         .then((response) {
       log(response.toString());
       return response;
+    });
+  }
+
+  Future<dynamic> postMethod(Map<String, dynamic> map, String url) async {
+    dio.options.headers['Content-Type'] = 'application/json';
+    return await dio
+        .post(url,
+            data: map,
+            options: Options(responseType: ResponseType.json, method: 'POST'))
+        .then((value) {
+      log(value.data.isString());
+      log(value.headers.toString());
+      log(value.statusCode.toString());
+      return value;
     });
   }
 }
