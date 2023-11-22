00cb7a7869f4e7b54b63512f8b3b8b42251c4041
Arvin Visi <arvinvisi03@gmail.com>
2023-11-22 18:28:34 +0330

    add register link

--- lib/components/api_constant.dart
+++ lib/components/api_constant.dart
@@ -2,6 +2,7 @@ class ApiConstant {
   static const baseDlUrl = 'https://techblog.sasansafari.com';
   static const baseUrl = 'https://techblog.sasansafari.com/Techblog/api/';
   static const getHomeItem = '${baseUrl}home/?command=index';
-  static const getArticleList = '$baseUrl/article/get.php?command=new&user_id=1';
-  
+  static const getArticleList =
+      '$baseUrl/article/get.php?command=new&user_id=1';
+  static const postRegister = '${baseUrl}register/action.php';
 }
