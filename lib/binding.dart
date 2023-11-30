3a7e14f4a6d7da419f4c32c4d22832acede49718
Arvin Visi <arvinvisi03@gmail.com>
2023-11-30 12:56:51 +0330

    add binding file

---
+++ lib/binding.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+import 'package:project111/controller/article_contorller.dart';
+import 'package:project111/controller/article_detail_contorller.dart';
+import 'package:project111/controller/register_controller.dart';
+
+class ArticleBinding implements Bindings {
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+
+    Get.put(ArticleController());
+    Get.lazyPut(() => ArticleDetailController());
+  }
+}
+
+class RegisterBinding implements Bindings {
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+
+    Get.put(RegisterController());
+  }
+}
