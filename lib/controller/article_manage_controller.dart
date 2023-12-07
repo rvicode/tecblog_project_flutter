40691b7b58b48b17a5014582a2d9672bf2bea6db
Arvin Visi <arvinvisi03@gmail.com>
2023-12-07 23:18:42 +0330

    add article_manage_controller file

---
+++ lib/controller/article_manage_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:project111/models/article_model.dart';
+
+class ArticleManageController extends GetxController {
+  RxList<ArticleModel> articleList = RxList.empty();
+
+  getArticleManaged() {}
+}
