eb3c6d23c788ec546ef1b40458208714426218de
Arvin Visi <arvinvisi03@gmail.com>
2023-12-30 19:33:38 +0330

    add articlePost in api_constant file

--- lib/components/api_constant.dart
+++ lib/components/api_constant.dart
@@ -1,10 +1,21 @@
-class ApiConstant {
+class ApiUrlConstant {
   static const baseDlUrl = 'https://techblog.sasansafari.com';
   static const baseUrl = 'https://techblog.sasansafari.com/Techblog/api/';
   static const getHomeItem = '${baseUrl}home/?command=index';
   static const getArticleList =
       '$baseUrl/article/get.php?command=new&user_id=1';
   static const postRegister = '${baseUrl}register/action.php';
+  static const articlePost = '${baseUrl}article/post.php';
   static const publishedByMe =
       '${baseUrl}article/get.php?command=published_by_me&user_id=';
+}
+
+class ApiAeticleKeyConstant {
+  static const title = 'title';
+  static const content = 'content';
+  static const catId = 'cat_id';
+  static const tagList = 'tag_list';
+  static const userId = 'user_id';
+  static const image = 'image';
+  static const command = 'command';
 }
